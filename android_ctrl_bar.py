7d945822779a324b853e707ffa662d6772360adb
Chris <oldclock@users.noreply.github.com>
2022-03-31 12:19:17 +0800

    Add more buttons

--- android_ctrl_bar.py
+++ android_ctrl_bar.py
@@ -25,8 +25,18 @@ def adbCmd(timeout_sec: int, command: str) -> bool:
 
 # Define the window's contents
 layout = [ [sg.Text("Press button")],
-           [sg.Button('Back'), sg.Button('Home'), sg.Button('Apps')],
-           [sg.Text("Status: "), sg.Text(size=(40,1), key='statusText')],
+           [sg.Button(size=(5,1), key='btnBACK', button_text='◀'),
+            sg.Button(size=(5,1), key='btnHOME', button_text='⏺'),
+            sg.Button(size=(5,1), key='btnAPP_SWITCH', button_text='⏹')],
+           [sg.Button(size=(5,1), key='btnPAGE_UP', button_text='PgUp'),
+            sg.Button(size=(5,1), key='btnDPAD_UP', button_text='↑'),
+            sg.Button(size=(5,1), key='btnPAGE_DOWN', button_text='PgDn'),
+            sg.Button(size=(5,1), key='btnVOLUME_UP', button_text='♪+')],
+           [sg.Button(size=(5,1), key='btnDPAD_LEFT', button_text='←'),
+            sg.Button(size=(5,1), key='btnDPAD_DOWN', button_text='↓'),
+            sg.Button(size=(5,1), key='btnDPAD_RIGHT', button_text='→'),
+            sg.Button(size=(5,1), key='btnVOLUME_DOWN', button_text='♪-')],
+           [sg.Text("Status: "), sg.Text(size=(30,1), key='statusText')],
          ]
 
 # Create the window
@@ -39,17 +49,38 @@ while True:
     if event == sg.WINDOW_CLOSED:
         break
     # Handle button events
-    elif event == 'Back':
-        window['statusText'].update('Input ' + event + ", executing... ")
+    elif event == 'btnBACK':
         adbCmd(5, "shell input keyevent BACK")
         window['statusText'].update('Input ' + event + ", done")
-    elif event == 'Home':
-        window['statusText'].update('Input ' + event + ", executing... ")
+    elif event == 'btnHOME':
         adbCmd(5, "shell input keyevent HOME")
         window['statusText'].update('Input ' + event + ", done")
-    elif event == 'Apps':
-        window['statusText'].update('Input ' + event + ", executing... ")
+    elif event == 'btnAPP_SWITCH':
         adbCmd(5, "shell input keyevent APP_SWITCH")
+        window['statusText'].update('Input ' + event + ", done")
+    elif event == 'btnPAGE_UP':
+        adbCmd(5, "shell input keyevent PAGE_UP")
+        window['statusText'].update('Input ' + event + ", done")
+    elif event == 'btnPAGE_DOWN':
+        adbCmd(5, "shell input keyevent PAGE_DOWN")
+        window['statusText'].update('Input ' + event + ", done")
+    elif event == 'btnDPAD_UP':
+        adbCmd(5, "shell input keyevent DPAD_UP")
+        window['statusText'].update('Input ' + event + ", done")
+    elif event == 'btnDPAD_DOWN':
+        adbCmd(5, "shell input keyevent DPAD_DOWN")
+        window['statusText'].update('Input ' + event + ", done")
+    elif event == 'btnDPAD_LEFT':
+        adbCmd(5, "shell input keyevent DPAD_LEFT")
+        window['statusText'].update('Input ' + event + ", done")
+    elif event == 'btnDPAD_RIGHT':
+        adbCmd(5, "shell input keyevent DPAD_RIGHT")
+        window['statusText'].update('Input ' + event + ", done")
+    elif event == 'btnVOLUME_UP':
+        adbCmd(5, "shell input keyevent VOLUME_UP")
+        window['statusText'].update('Input ' + event + ", done")
+    elif event == 'btnVOLUME_DOWN':
+        adbCmd(5, "shell input keyevent VOLUME_DOWN")
         window['statusText'].update('Input ' + event + ", done")
 
 # Finish up by removing from the screen
